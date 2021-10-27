1b0805ff091a9eb0b31fb6a588ee97e9df5662f1
sam <s.gelernter@gmail.com>
2021-10-27 11:45:09 -0400

    Begin phase 3

--- error_handling_funtime_skeleton/super_useful.rb
+++ error_handling_funtime_skeleton/super_useful.rb
@@ -1,3 +1,6 @@
+class CoffeeError < StandardError; end
+class NotFruitError < StandardError; end
+
 # PHASE 2
 def convert_to_int(str)
   begin
@@ -15,19 +18,23 @@ def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
+    raise CoffeeError.new("that's not a fruit, but thank you for the coffee?") if maybe_fruit == "coffee"
+    raise NotFruitError.new("that's not a fruit")
   end 
-end
+  end
 
-def feed_me_a_fruit
+  def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   begin 
     maybe_fruit = gets.chomp
     reaction(maybe_fruit) 
-  rescue
+  rescue CoffeeError => e
+    puts e.message
     retry if maybe_fruit == 'coffee'
+  rescue NotFruitError => e
+    puts e.message 
   end
 end  
 
@@ -36,11 +43,12 @@ class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
     @yrs_known = yrs_known
+    raise "You don't know me like that bruh" if @yrs_known < 5
     @fav_pastime = fav_pastime
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known}."
   end
 
   def do_friendstuff
